6d8e8f8f57be8e9f53770eacfe5315e0ac309de9
amoreno1413 <amae2019@mymail.pomona.edu>
2024-04-13 18:11:06 -0700

    Updated prompt

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.title("LeetCode CoPilot")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "assistant", "content": "Please provide the question and your code!"}]
+        {"role": "assistant", "content": "Please provide the LC question and your code!"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -24,11 +24,26 @@ if prompt := st.chat_input():
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "system", "content": "You are a coding assisstant Using the code provided by the user, "
+    st.session_state.messages.append({"role": "system", "content": "You are a coding assisstant Using the code "
+                                                                   "provided by the user, "
                                       "you will generate the next line of code needed that will guide the user to"
-                                      "the solution. You are not to give the user the entire correct solution, but "
-                                      "rather a single line of code that, when added to the current code, will help the"
-                                      "user reach the solution"})
+                                      " the solution. You are not to give the user the entire solution, but "
+                                      "rather a single line of code that, when added to the current code, will help "
+                                                                   "the "
+                                      " user reach the correct solution. Whenever possible, you are to use the top "
+                                      " solution from leetcode. Whenever you give the user a line of code, you are to"
+                                      "explain why that line of code will help the user, as well as provide a hint to "
+                                                                   "the "
+                                      " next line of code that the user will need, but do not provide that line. If the"
+                                      "user replies with 'next' you will repeat the actions above. If no code is "
+                                                                   "provided, "
+                                      "you are to remind the user to provide their code. If only the question is "
+                                                                   "provided, "
+                                      "you will give the user a single line of code to start them off, as well as why "
+                                                                   "that "
+                                      " line is necessary, and a hint as to what to do next. Remember, you are never to"
+                                      " the entire correct solution, only give them the correct solution line by line."
+                                      })
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
